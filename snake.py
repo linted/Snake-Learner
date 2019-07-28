ec34df1455ddbce3fd15f1085f702273aba3d70d
linted <linted@users.noreply.github.com>
2019-07-28 17:03:00 -0400

    started working on getting everything integrated

--- snake.py
+++ snake.py
@@ -25,40 +25,46 @@ snake = [
 food = [sh/2, sw/2]
 w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
 
-key = curses.KEY_RIGHT
 
-while True:
-    next_key = w.getch()
-    key = key if next_key == -1 else next_key
+def play():
+    key = curses.KEY_RIGHT
+    while True:
+        next_key = w.getch()
+        key = key if next_key == -1 else next_key
 
-    if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
-        curses.endwin()
-        quit()
+        if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
+            curses.endwin()
+            quit()
 
-    new_head = [snake[0][0], snake[0][1]]
+    move_snake(key)
 
-    if key == curses.KEY_DOWN:
-        new_head[0] += 1
-    if key == curses.KEY_UP:
-        new_head[0] -= 1
-    if key == curses.KEY_LEFT:
-        new_head[1] -= 1
-    if key == curses.KEY_RIGHT:
-        new_head[1] += 1
+def move_snake(direction):
+        new_head = [snake[0][0], snake[0][1]]
 
-    snake.insert(0, new_head)
+        if direction == curses.KEY_DOWN:
+            new_head[0] += 1
+        elif  == curses.KEY_UP:
+            new_head[0] -= 1
+        elif direction == curses.KEY_LEFT:
+            new_head[1] -= 1
+        elif direction == curses.KEY_RIGHT:
+            new_head[1] += 1
+        else:
+            raise NotImplemented("Invalid Direction")
 
-    if snake[0] == food:
-        food = None
-        while food is None:
-            nf = [
-                random.randint(1, sh-1),
-                random.randint(1, sw-1)
-            ]
-            food = nf if nf not in snake else None
-        w.addch(food[0], food[1], curses.ACS_PI)
-    else:
-        tail = snake.pop()
-        w.addch(int(tail[0]), int(tail[1]), ' ')
+        snake.insert(0, new_head)
 
-w.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
+        if snake[0] == food:
+            food = None
+            while food is None:
+                nf = [
+                    random.randint(1, sh-1),
+                    random.randint(1, sw-1)
+                ]
+                food = nf if nf not in snake else None
+            w.addch(food[0], food[1], curses.ACS_PI)
+        else:
+            tail = snake.pop()
+            w.addch(int(tail[0]), int(tail[1]), ' ')
+
+        w.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
