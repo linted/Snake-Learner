cba8262587b02a8f1fa4c251c864fb3618f68f4b
linted <linted@users.noreply.github.com>
2019-08-07 21:28:16 -0400

    formatted with black

--- snake.py
+++ snake.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-'''
+"""
 Credit to engineer-man <github@engineerman.org> for original snake game code.
-'''
+"""
 
 import curses
 import random
@@ -16,19 +16,13 @@ w = curses.newwin(sh, sw, 0, 0)
 w.keypad(1)
 w.timeout(100)
 
-snk_x = sw//4
-snk_y = sh//2
-snake = [
-    [snk_y, snk_x],
-    [snk_y, snk_x-1],
-    [snk_y, snk_x-2]
-]
-
-
+snk_x = sw // 4
+snk_y = sh // 2
+snake = [[snk_y, snk_x], [snk_y, snk_x - 1], [snk_y, snk_x - 2]]
 
 
 def play():
-    food = [sh//2, sw//2]
+    food = [sh // 2, sw // 2]
     w.addch(*food, curses.ACS_PI)
 
     key = curses.KEY_RIGHT
@@ -37,14 +31,13 @@ def play():
         next_key = w.getch()
         key = key if next_key == -1 else next_key
 
-        if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
+        if snake[0][0] in [0, sh] or snake[0][1] in [0, sw] or snake[0] in snake[1:]:
             curses.endwin()
             quit()
-    
 
         if key == curses.KEY_DOWN:
             direction = 0
-        elif key== curses.KEY_UP:
+        elif key == curses.KEY_UP:
             direction = 1
         elif key == curses.KEY_LEFT:
             direction = 2
@@ -52,7 +45,6 @@ def play():
             direction = 3
 
         move_snake(direction, food)
-
 
 
 def move_snake(direction, goal):
@@ -72,17 +64,15 @@ def move_snake(direction, goal):
     if snake[0] == goal:
         food = None
         while food is None:
-            nf = [
-                random.randint(1, sh-1),
-                random.randint(1, sw-1)
-            ]
+            nf = [random.randint(1, sh - 1), random.randint(1, sw - 1)]
             food = nf if nf not in snake else None
         w.addch(*food, curses.ACS_PI)
     else:
         tail = snake.pop()
-        w.addch(*tail, ' ')
+        w.addch(*tail, " ")
 
     w.addch(*snake[0], curses.ACS_CKBOARD)
+
 
 if __name__ == "__main__":
     try:
